7b26b5760ea77ccab28f9ebef86c30e27a2dd5ca
asifaminisonline <asifaminisonline@gmail.com>
2022-12-01 01:22:08 +0530

    Add navigation functionality to navbar

--- main.js
+++ main.js
@@ -110,4 +110,38 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
 });
 
-// remove book from store
+function showSec(section) {
+  const secBookList = document.getElementById('list');
+  const secBookForm = document.getElementById('add-book');
+  const secContact = document.getElementById('contact');
+
+  switch (section) {
+    case 'list':
+      if (secBookList.classList.contains('d-none')) {
+        secBookList.classList.remove('d-none');
+        secBookForm.classList.add('d-none');
+        secContact.classList.add('d-none');
+      }
+      break;
+
+    case 'form':
+      if (secBookForm.classList.contains('d-none')) {
+        secBookForm.classList.remove('d-none');
+        secBookList.classList.add('d-none');
+        secContact.classList.add('d-none');
+      }
+      break;
+
+    case 'contact':
+      if (secContact.classList.contains('d-none')) {
+        secContact.classList.remove('d-none');
+        secBookForm.classList.add('d-none');
+        secBookList.classList.add('d-none');
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+showSec();
