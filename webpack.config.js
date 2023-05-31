52d9fcd062bc5dccb97d65c89c0e3fd8666a1022
asifaminisonline <asifaminisonline@gmail.com>
2023-06-01 00:38:25 +0530

    Add and configure webpack to the project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
